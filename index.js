daeed885d3bac0329d821d0491513231bb73f16b
Shane Meyers <smeyers1031@gmail.com>
2022-08-06 12:16:04 -0700

    commit tree 254d3cb22dbe2c74a2f4091c01b0ea191cd1ed97

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() + 1);
+
+let num3 = 24;
+let num4 = 5;
+const mod = num3 % num4;
+
+let max = Math.max(1, 7, 13, 18, 20);
